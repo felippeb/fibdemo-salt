aaa4130d284ff2e0615691280f870894a6f4635d
felippe burk <felippe.burk@emc.com>
2016-01-31 17:39:44 -0700

    updating jenkins

--- saltbase/gcloud/files/gcloudinstall.sh
+++ saltbase/gcloud/files/gcloudinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Authenticate
-gcloud auth activate-service-account --key-file /opt/gcloud/fibdemo-a96cb930a7c2.json
+gcloud auth activate-service-account --key-file /opt/gcloud/fibdemo-auth.json
